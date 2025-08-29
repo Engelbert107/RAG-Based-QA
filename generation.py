32e7098e77031637bf108296d1910a644448952c
Engelbert107 <etchinde@gmail.com>
2025-08-29 00:30:22 -0400

    comment the updated version

--- generation.py
+++ generation.py
@@ -1,6 +1,7 @@
 
 import torch
 from transformers import pipeline
+from helper import truncate_by_tokens
 
 # Text-generation pipeline using TinyLlama (CPU)
 pipe = pipeline(
@@ -10,7 +11,8 @@ pipe = pipeline(
     torch_dtype=torch.float32    # CPU-safe dtype
 )
 
-def generate_text(prompt: str, max_new_tokens: int = 150) -> str:
+
+def generate_text(prompt: str, max_new_tokens: int = 256) -> str:
     """
     Generate text from a given prompt using TinyLlama.
     """
@@ -22,3 +24,21 @@ def generate_text(prompt: str, max_new_tokens: int = 150) -> str:
         top_p=0.95
     )
     return outputs[0]["generated_text"]
+
+
+# def generate_text(prompt: str, max_new_tokens: int = 150) -> str:
+#     """
+#     Generate text from a given prompt using TinyLlama, handling long prompts safely.
+#     """
+#     # Truncate prompt to prevent exceeding model max length
+#     safe_prompt = truncate_by_tokens(prompt, max_tokens= 2048)
+    
+#     outputs = pipe(
+#         safe_prompt,
+#         max_new_tokens=max_new_tokens,
+#         do_sample=True,
+#         temperature=0.1,
+#         top_p=0.95
+#     )
+#     return outputs[0]["generated_text"]
+
